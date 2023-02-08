9ef7e28f1e5db285301c994f0102d505a73ab4f2
Larus <larusantunes.luz@hotmail.com>
2023-02-08 17:57:33 -0300

    Atividade Encontro Remoto 02

--- UC12_BackEnd/Program.cs
+++ UC12_BackEnd/Program.cs
@@ -1,3 +1,10 @@
 ﻿using UC12_BackEnd.Classes;
 
 PessoaFisica novaPf= new PessoaFisica();
+novaPf.nome = "Joao";
+novaPf.cpf = "010.010.010-01";
+
+// Console.WriteLine(novaPf.nome);
+// Console.WriteLine(novaPf.cpf);
+
+Console.WriteLine($"Olá {novaPf.nome} de CPF: {novaPf.cpf}");
